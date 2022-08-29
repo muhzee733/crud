1a5d3c74a7246d44595c0f013c9ab37336577f24
ghulamabbaas <abaxi077@gmail.com>
2022-08-29 12:18:31 +0500

    code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,94 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import React from 'react'
+import data from './data';
+import './App.css';
+import { useState, useEffect } from 'react';
+
+const App = (Value) => {
+  const [pro,setPro]=useState([]);
+  const [model, setModel] = useState(false);
+  const [add, setAddPopup] = useState(false);
+  // Add User
+  const [name, setName] = useState('');
+
+  useEffect(()=>{
+    setPro(data)
+    
+    },[]);
+
+// Popup
+  const dataEdit = () =>{
+      setModel(!model);
+  }
+  const addUser = () =>{
+    setAddPopup(!add);
+  }
+  // Function
+  const userNewUser = () =>{
+    console.log(name)
+  } 
+
+  const deletUser = (id) =>{
+    console.log(id)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    <button onClick={addUser}>Add User</button>
+    <table>
+      <tr>
+        <th>id:</th>
+        <th>Name</th>
+        <th>Edu</th>
+        <th>Edit</th>
+        <th>Delet</th>
+        <th>Update</th>
+      </tr>
+
+      {
+        data.map((val, i) =>(
+          <>
+            <tr key={i}>
+              <td key={i}>{val.id}</td>
+              <td>{val.name}</td>
+              <td>{val.edu}</td>
+              <td><button onClick={dataEdit}>Edit</button></td>
+              <td><button onClick={() => deletUser(val.id)}>Delete</button></td>
+              <td><button>Update</button></td>
+            </tr>
+          </>
+        ))
+      }
+
+      </table>
+      {
+        model && (
+          <div className='model'>
+            <h1>Edit Querry</h1>
+            <tr>
+              <td>Id:</td>
+              <td><input type="text" /></td>
+              <td><input type="text" /></td>
+              <td><button>Update</button></td>
+            </tr>
+          </div>
+        )
+      }
+      {
+        add && (
+          <div className='addUser'>
+            <h1>Add User</h1>
+            <tr>
+              <td><input value={name} type="text" onChange={(e) => setName(e.target.value)}/></td>
+              <td><input type="text" /></td>
+              <td><button onClick={userNewUser}>Add User</button></td>
+            </tr>
+          </div>
+        )
+      }
+
+  </>
+  )
 }
 
-export default App;
+export default App
---
+++ src/data.jsx
@@ -0,0 +1,54 @@
+const  dataFile = [
+    {
+        id: '1',
+        name: 'Ali',
+        edu: 'MA',
+    },
+    {
+        id: '2',
+        name: 'Ali',
+        edu: 'MA',
+    },
+    {
+        id: '3',
+        name: 'Ali',
+        edu: 'MA',
+    },
+    {
+        id: '4',
+        name: 'Ali',
+        edu: 'MA',
+    },
+    {
+        id: '5',
+        name: 'Ali',
+        edu: 'MA',
+    },
+    {
+        id: '6',
+        name: 'Ali',
+        edu: 'MA',
+    },
+    {
+        id: '7',
+        name: 'Ali',
+        edu: 'MA',
+    },
+    {
+        id: '8',
+        name: 'Ali',
+        edu: 'MA',
+    },
+    {
+        id: '9',
+        name: 'Ali',
+        edu: 'MA',
+    },
+    {
+        id: '10',
+        name: 'Ali',
+        edu: 'MA',
+    },
+]
+
+export default dataFile;
